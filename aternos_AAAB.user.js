4571138f5e50242268e5a7d79ccee0146aff5076
Doxca <81829364+Doxca@users.noreply.github.com>
2021-07-30 09:47:11 +0200

    Update and rename aternos_antiadblock.js to aternos_AAAB.user.js

--- aternos_AAAB.user.js
+++ aternos_AAAB.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name        Aternos anti-anti adblock
 // @description Kills Aternos' annoying full-page + timeout anti-adblock.
-// @version     1.0.3
+// @version     1.0.4
 // @author      Doxca
 // @match       *://aternos.org/*
 // @grant       none
 // @inject-into page
 // @run-at      document-start
-// @namespace       https://github.com/Doxca/anti-adblock-aternos
-// @updateURL       https://raw.githubusercontent.com/Doxca/anti-adblock-aternos/main/aternos_antiadblock.js
-// @downloadURL     https://raw.githubusercontent.com/Doxca/anti-adblock-aternos/main/aternos_antiadblock.js
-// @supportURL      https://github.com/Doxca/anti-adblock-aternos/issues
+// @namespace       https://github.com/Doxca/aternos_AAAB
+// @updateURL       https://raw.githubusercontent.com/Doxca/aternos_AAAB/main/aternos_AAAB.user.js
+// @downloadURL     https://raw.githubusercontent.com/Doxca/aternos_AAAB/main/aternos_AAAB.user.js
+// @supportURL      https://github.com/Doxca/aternos_AAAB/issues
 // @meta        Thank you aspen, i juste modifyed a little thing but it's a modification so .. idk credit to you thx
 // ==/UserScript==
 
