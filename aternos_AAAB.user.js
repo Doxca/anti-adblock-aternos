cdff106d9b34481a3e7b65e3d14f7fd85c92cd0f
Ender <81829364+Endxca@users.noreply.github.com>
2022-06-15 00:52:28 +0200

    Update aternos_AAAB.user.js

--- aternos_AAAB.user.js
+++ aternos_AAAB.user.js
@@ -2,15 +2,15 @@
 // @name        Aternos anti-anti adblock
 // @description Kills Aternos' annoying full-page + timeout anti-adblock.
 // @version     1.0.4
-// @author      Doxca
+// @author      Ender
 // @match       *://aternos.org/*
 // @grant       none
 // @inject-into page
 // @run-at      document-start
-// @namespace       https://github.com/Doxca/aternos_AAAB
-// @updateURL       https://raw.githubusercontent.com/Doxca/aternos_AAAB/main/aternos_AAAB.user.js
-// @downloadURL     https://raw.githubusercontent.com/Doxca/aternos_AAAB/main/aternos_AAAB.user.js
-// @supportURL      https://github.com/Doxca/aternos_AAAB/issues
+// @namespace       https://github.com/Endxca/aternos_AAAB
+// @updateURL       https://raw.githubusercontent.com/Endxca/aternos_AAAB/main/aternos_AAAB.user.js
+// @downloadURL     https://raw.githubusercontent.com/Endxca/aternos_AAAB/main/aternos_AAAB.user.js
+// @supportURL      https://github.com/Endxca/aternos_AAAB/issues
 // @meta        Thank you aspen, i juste modifyed a little thing but it's a modification so .. idk credit to you thx
 // ==/UserScript==
 
